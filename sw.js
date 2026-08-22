6608133faf1c9bd66b22490311e5bc5ba10e944f
hayato2838810-art <hayato2838810@gmail.com>
2026-08-22 23:29:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='kaji-share-v5';
-const A=['./','index.html','chores.html','manifest.webmanifest'];
+const C='kaji-share-v7';
+const A=['./','index.html','settings.html','chores.html','manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)))});

eadfa8c127ea3d814c8716c487c511f0ff47693f
hayato2838810-art <hayato2838810@gmail.com>
2026-08-22 23:19:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,5 @@
-const CACHE='kaji-share-v4-fixed';
-const ASSETS=['./','index.html','manifest.webmanifest'];
-
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    Promise.all([
-      self.clients.claim(),
-      caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-    ])
-  );
-});
-
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    fetch(e.request)
-      .then(r=>{
-        const copy=r.clone();
-        caches.open(CACHE).then(c=>c.put(e.request,copy));
-        return r;
-      })
-      .catch(()=>caches.match(e.request))
-  );
-});
+const C='kaji-share-v5';
+const A=['./','index.html','chores.html','manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))]))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)))});
